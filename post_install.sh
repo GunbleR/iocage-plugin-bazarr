f1211d5ebbd34ed781698bc3d599d2dbaf583717
fulder <misad90@gmail.com>
2020-03-15 22:00:54 +0100

    Correct echo command

--- post_install.sh
+++ post_install.sh
@@ -3,7 +3,7 @@
 mkdir -p /usr/local/etc/rc.d
 
 version="0.8.4.2"
-echo "Downloading bazarr verion"
+echo "Downloading bazarr version: ${version}"
 fetch "https://github.com/morpheus65535/bazarr/archive/v${version}.tar.gz"
 tar xfz "v${version}.tar.gz" -C /usr/local/
 mv "/usr/local/bazarr-${version}" /usr/local/bazarr
