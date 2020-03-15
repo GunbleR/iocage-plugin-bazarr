8386472fa6e6c1abea59f3705feb7baca19aca6d
fulder <misad90@gmail.com>
2020-03-15 17:13:39 +0100

    Enable and start service in post_install script

--- post_install.sh
+++ post_install.sh
@@ -5,3 +5,9 @@ tar xfz v0.8.4.2.tar.gz -C /usr/local/
 mv /usr/local/bazarr-0.8.4.2 /usr/local/bazarr
 
 pip install -r /usr/local/bazarr/requirements.txt --user
+
+# Enable bazarr service
+sysrc -f /etc/rc.conf bazarr_enable="YES"
+
+# Start service
+service bazarr start
