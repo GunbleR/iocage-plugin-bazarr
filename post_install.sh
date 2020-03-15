1a7e0303eec60521d76d0d6059187aa7390c6241
fulder <misad90@gmail.com>
2020-03-15 22:02:21 +0100

    Add more echo messages in post_install

--- post_install.sh
+++ post_install.sh
@@ -3,20 +3,23 @@
 mkdir -p /usr/local/etc/rc.d
 
 version="0.8.4.2"
+user_name="bazarr"
+
 echo "Downloading bazarr version: ${version}"
 fetch "https://github.com/morpheus65535/bazarr/archive/v${version}.tar.gz"
 tar xfz "v${version}.tar.gz" -C /usr/local/
 mv "/usr/local/bazarr-${version}" /usr/local/bazarr
 
+echo "Installing bazarr requirements"
 pip install -r /usr/local/bazarr/requirements.txt --user
 
-# Create bazarr user
-pw useradd -n bazarr -c "Bazarr" -s /sbin/nologin -w no
-chown -R bazarr /usr/local/bazarr
+echo "Creating new user with name: {user_name}"
+pw useradd -n ${user_name} -c "Bazarr" -s /sbin/nologin -w no
+chown -R ${user_name}  /usr/local/bazarr
 
-# Enable bazarr service
+echo "Start bazarr service"
 chmod +x /usr/local/etc/rc.d/bazarr
 sysrc -f /etc/rc.conf bazarr_enable="YES"
-
-# Start service
 service bazarr start
+
+echo "Post install completed!"
