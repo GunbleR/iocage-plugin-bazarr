5465737fe99b0638d3bb88487aa4967a5719d391
fulder <misad90@gmail.com>
2020-03-15 22:00:24 +0100

    Move out version to a variable

--- post_install.sh
+++ post_install.sh
@@ -2,9 +2,11 @@
 
 mkdir -p /usr/local/etc/rc.d
 
-fetch https://github.com/morpheus65535/bazarr/archive/v0.8.4.2.tar.gz
-tar xfz v0.8.4.2.tar.gz -C /usr/local/
-mv /usr/local/bazarr-0.8.4.2 /usr/local/bazarr
+version="0.8.4.2"
+echo "Downloading bazarr verion"
+fetch "https://github.com/morpheus65535/bazarr/archive/v${version}.tar.gz"
+tar xfz "v${version}.tar.gz" -C /usr/local/
+mv "/usr/local/bazarr-${version}" /usr/local/bazarr
 
 pip install -r /usr/local/bazarr/requirements.txt --user
 
