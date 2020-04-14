8f7b8ee5a96ea375098645f2b21d0ecb69e9d61f
fulder <misad90@gmail.com>
2020-04-15 00:02:47 +0200

    Add extra echo

--- migration/0.0.2.sh
+++ migration/0.0.2.sh
@@ -6,6 +6,8 @@ git_url=https://github.com/morpheus65535/bazarr.git
 
 echo "Setting missing git remote to ${git_url}"
 git -C /usr/local/bazarr remote set-url origin ${git_url}
+
+echo "Reset bazarr to latest master"
 git -C /usr/local/bazarr fetch
 git -C /usr/local/bazarr branch --set-upstream-to=origin/master master
 git -C /usr/local/bazarr reset --hard origin/master
