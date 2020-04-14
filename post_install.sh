ba31806f08979269ecace17e45897df16f3868ed
fulder <misad90@gmail.com>
2020-04-14 17:46:41 +0200

    Clone git repo instead of downloading tar artifact

--- post_install.sh
+++ post_install.sh
@@ -2,13 +2,10 @@
 
 mkdir -p /usr/local/etc/rc.d
 
-version="0.8.4.2"
 user_name="bazarr"
 
-echo "Downloading bazarr version: ${version}"
-fetch "https://github.com/morpheus65535/bazarr/archive/v${version}.tar.gz"
-tar xfz "v${version}.tar.gz" -C /usr/local/
-mv "/usr/local/bazarr-${version}" /usr/local/bazarr
+echo "Clone bazarr repo"
+git clone https://github.com/morpheus65535/bazarr.git /usr/local/bazarr
 
 echo "Installing bazarr requirements"
 pip install -r /usr/local/bazarr/requirements.txt
