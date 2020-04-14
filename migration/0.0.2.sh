2c80b762f2be67f9175580aef00354eb6f39af29
fulder <misad90@gmail.com>
2020-04-15 00:01:57 +0200

    Set upstream to master, clean untracked files and add 'set -e'

--- migration/0.0.2.sh
+++ migration/0.0.2.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
+set -e
+
 git_url=https://github.com/morpheus65535/bazarr.git
 
 echo "Setting missing git remote to ${git_url}"
 git -C /usr/local/bazarr remote set-url origin ${git_url}
 git -C /usr/local/bazarr fetch
+git -C /usr/local/bazarr branch --set-upstream-to=origin/master master
 git -C /usr/local/bazarr reset --hard origin/master
+git -C /usr/local/bazarr clean -f -d
 
 echo "Installing requirements"
 pip install -U -r /usr/local/bazarr/requirements.txt
