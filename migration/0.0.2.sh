eea8f3cd7dbc5b7c2668ef8cd0ce9837770f5aa8
fulder <misad90@gmail.com>
2020-04-14 20:07:23 +0200

    Add echo to migration 0.0.2

--- migration/0.0.2.sh
+++ migration/0.0.2.sh
@@ -1,1 +1,6 @@
-git -C /usr/local/bazarr remote add origin https://github.com/morpheus65535/bazarr.git
+#!/bin/sh
+
+git_url=https://github.com/morpheus65535/bazarr.git
+
+echo "Setting missing git remote to ${git_url}"
+git -C /usr/local/bazarr remote add origin ${git_url}
