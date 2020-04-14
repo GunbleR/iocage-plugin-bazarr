aa273179eeb08974c65fdd0028bb7e645875f90d
fulder <misad90@gmail.com>
2020-04-14 20:21:41 +0200

    Change add origin to set-url origin

--- migration/0.0.2.sh
+++ migration/0.0.2.sh
@@ -3,7 +3,7 @@
 git_url=https://github.com/morpheus65535/bazarr.git
 
 echo "Setting missing git remote to ${git_url}"
-git -C /usr/local/bazarr remote add origin ${git_url}
+git -C /usr/local/bazarr remote set-url origin ${git_url}
 
 echo "Installing requirements"
 pip install -U -r /usr/local/bazarr/requirements.txt
