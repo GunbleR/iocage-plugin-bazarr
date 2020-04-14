331563e613eafe04b755ddaaf855d00f19fc729d
fulder <misad90@gmail.com>
2020-04-15 00:04:45 +0200

    Run git init if needed

--- migration/0.0.2.sh
+++ migration/0.0.2.sh
@@ -4,6 +4,9 @@ set -e
 
 git_url=https://github.com/morpheus65535/bazarr.git
 
+echo "Init git if needed"
+git -C /usr/local/bazarr init
+
 echo "Setting missing git remote to ${git_url}"
 git -C /usr/local/bazarr remote set-url origin ${git_url}
 
