9bfb03cd774dd75dbc9aaffb572ea982221c7b09
fulder <misad90@gmail.com>
2020-04-14 23:43:55 +0200

    Reset to origin/master branch after setting remote URL

--- migration/0.0.2.sh
+++ migration/0.0.2.sh
@@ -4,6 +4,7 @@ git_url=https://github.com/morpheus65535/bazarr.git
 
 echo "Setting missing git remote to ${git_url}"
 git -C /usr/local/bazarr remote set-url origin ${git_url}
+git reset --hard origin/master
 
 echo "Installing requirements"
 pip install -U -r /usr/local/bazarr/requirements.txt
