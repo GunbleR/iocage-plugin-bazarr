1ebd62019705560215332fe1a5dda3a5f2af2862
fulder <misad90@gmail.com>
2020-04-14 23:50:22 +0200

    Forgotten git -C flag

--- migration/0.0.2.sh
+++ migration/0.0.2.sh
@@ -4,8 +4,8 @@ git_url=https://github.com/morpheus65535/bazarr.git
 
 echo "Setting missing git remote to ${git_url}"
 git -C /usr/local/bazarr remote set-url origin ${git_url}
-git fetch
-git reset --hard origin/master
+git -C /usr/local/bazarr fetch
+git -C /usr/local/bazarr reset --hard origin/master
 
 echo "Installing requirements"
 pip install -U -r /usr/local/bazarr/requirements.txt
