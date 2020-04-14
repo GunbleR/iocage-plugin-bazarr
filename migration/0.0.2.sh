84dc91dc37fbf72a08cbbe1e0a346732b515bc6b
fulder <misad90@gmail.com>
2020-04-14 20:11:38 +0200

    Update and install requirments
    If update plugin happens when bazarr released some large changes
    with new requirements this could be needed.

--- migration/0.0.2.sh
+++ migration/0.0.2.sh
@@ -4,3 +4,6 @@ git_url=https://github.com/morpheus65535/bazarr.git
 
 echo "Setting missing git remote to ${git_url}"
 git -C /usr/local/bazarr remote add origin ${git_url}
+
+echo "Installing requirements"
+pip install -U -r /usr/local/bazarr/requirements.txt
