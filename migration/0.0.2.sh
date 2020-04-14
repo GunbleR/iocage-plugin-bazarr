c2fd3f1215796174e0d1f637468190aeb3bb7f75
fulder <misad90@gmail.com>
2020-04-15 00:25:05 +0200

    Re-init git if needed and use remote add origin command

--- migration/0.0.2.sh
+++ migration/0.0.2.sh
@@ -4,15 +4,16 @@ set -e
 
 git_url=https://github.com/morpheus65535/bazarr.git
 
-echo "Init git if needed"
+echo "Re-init git if needed"
+rm -rf /usr/local/bazarr/.git
 git -C /usr/local/bazarr init
 
 echo "Setting missing git remote to ${git_url}"
-git -C /usr/local/bazarr remote set-url origin ${git_url}
+git -C /usr/local/bazarr remote add origin ${git_url}
 
 echo "Reset bazarr to latest master"
 git -C /usr/local/bazarr fetch
-git -C /usr/local/bazarr branch --set-upstream-to=origin/master master
+git -C /usr/local/bazarr checkout -b master origin/master
 git -C /usr/local/bazarr reset --hard origin/master
 git -C /usr/local/bazarr clean -f -d
 
