bb7c697730c556fad4e88b26e2ba6dd9a1b2ce36
fulder <misad90@gmail.com>
2020-04-15 01:23:29 +0200

    Move restarting to the end of post_update.sh

--- overlay/usr/local/migration/0.0.2.sh
+++ overlay/usr/local/migration/0.0.2.sh
@@ -24,6 +24,3 @@ pip install -U -r /usr/local/bazarr/requirements.txt
 
 echo "Cleanup"
 rm -f /v0.8.4.2.tar.gz
-
-echo "Restart bazarr"
-service bazarr restart
--- post_update.sh
+++ post_update.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
 ./usr/local/migration//0.0.2.sh
+
+echo "Restart bazarr"
+service bazarr stop
+service bazarr start
