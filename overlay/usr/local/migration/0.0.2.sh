193909e20d853ead19feb0b6fdeefec47bb0c820
fulder <misad90@gmail.com>
2020-04-15 01:20:37 +0200

    Fix echo messages in 0.0.2 migration

--- overlay/usr/local/migration/0.0.2.sh
+++ overlay/usr/local/migration/0.0.2.sh
@@ -8,13 +8,15 @@ echo "Re-init git"
 rm -rf /usr/local/bazarr/.git
 git -C /usr/local/bazarr init
 
-echo "Setting missing git remote to ${git_url}"
+echo "Setting git remote to ${git_url}"
 git -C /usr/local/bazarr remote add origin ${git_url}
 
 echo "Reset bazarr to latest master"
 git -C /usr/local/bazarr fetch
 git -C /usr/local/bazarr checkout -b master origin/master
 git -C /usr/local/bazarr reset --hard origin/master
+
+echo "Git cleanup"
 git -C /usr/local/bazarr clean -f -d
 
 echo "Installing requirements"
