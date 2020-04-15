df37be21d78a1a3e1146a8fd5246e514c7b2447e
fulder <misad90@gmail.com>
2020-04-15 20:58:57 +0200

    Disable service in pre_update

--- post_update.sh
+++ post_update.sh
@@ -3,4 +3,5 @@
 ./usr/local/migration/0.0.2.sh
 ./usr/local/migration/0.0.3.sh
 
+sysrc -f /etc/rc.conf bazarr_enable="TES"
 service bazarr start
--- pre_update.sh
+++ pre_update.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
 service bazarr stop | true
+sysrc -f /etc/rc.conf bazarr_enable="NO"
