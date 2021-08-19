b33537f1a73c239d99daadcbbcae3b8b1010bd5e
fulder <misad90@gmail.com>
2021-08-19 23:42:21 +0200

    Overrite zip files don't remove data files

--- overlay/usr/local/migration/0.3.0.sh
+++ overlay/usr/local/migration/0.3.0.sh
@@ -2,13 +2,11 @@
 
 echo "Running migration for 0.3.0"
 
-rm -r /usr/local/bazarr
-
 echo "Fetch ZIP"
 fetch https://github.com/morpheus65535/bazarr/releases/latest/download/bazarr.zip
 
 echo "Unzip bazarr archive"
-unzip -d /usr/local/bazarr bazarr.zip
+unzip -o -d /usr/local/bazarr bazarr.zip
 
 user_name="bazarr"
 chown -R ${user_name} /usr/local/bazarr
