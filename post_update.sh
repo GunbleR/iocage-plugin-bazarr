607d789691af4be9408fcce9a186b1df82f72bf2
fulder <misad90@gmail.com>
2021-08-20 20:24:56 +0200

    Rename migration to correct tag

--- post_update.sh
+++ post_update.sh
@@ -3,6 +3,6 @@
 ./usr/local/migration/0.1.0.sh
 ./usr/local/migration/0.2.0.sh
 ./usr/local/migration/0.2.1.sh
-./usr/local/migration/0.3.0.sh
+./usr/local/migration/1.0.1.sh
 
 sysrc -f /etc/rc.conf bazarr_enable="YES"
